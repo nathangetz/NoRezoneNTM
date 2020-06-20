80f890e4200919f5feca56706b1348bced430159
Nathan Getz <2036851@jeffcoschools.us>
2020-06-20 05:08:32 -0600

    Switched to form footer

--- footer.php
+++ footer.php
@@ -1,16 +1,40 @@
+            </div>
+        </main>
+        <footer class="pagefoot mt-auto footer pt-3 text-center bg-dark">
+            <div class="container-lg">
+            <?php if (function_exists('customPageFooter')){
+            customPageFooter();
+            }?>
+            <form method="POST" action="mail.php" class="py-3 needs-validation" novalidate>
+                <h3 class="text-light font-weight-lighter">To receive info and updates, subscribe to the No Rezone NTM mailing list:</h3>
+                <div class="input-group form-check my-3">
+                    <input class="form-control" name="email-address" type="email"  size="64" maxLength="64" required placeholder="Email address..." aria-label="Email address" pattern=".+@." title="Enter a valid email address.">
+                    <div class="input-group-append">
+                        <button class="btn btn-primary" type="submit"><span class="mx-2 mx-sm-3 mx-md-4 mx-lg-5">Subscribe</span></button>
+                    </div>
+                    <div class="invalid-feedback">Please enter a valid email address!</div>
                 </div>
-            </main>
-            <footer class="pagefoot mt-auto footer pt-3 text-center bg-dark">
-                <div class="container-lg">
-                <?php if (function_exists('customPageFooter')){
-                customPageFooter();
-                }?>
-                    <h3 class="text-light font-weight-light mt-5 mb-3">To receive info and updates:</h3>
-                    <a href="mailto:kristen@norezonentm.com?subject=Subscribe%20to%20the%20No%20Rezone%20NTM%20Mailing%20List&body=Send%20this%20email%20with%20the%20address%20you%20would%20like%20added%20to%20the%20mailing%20list." class="btn btn-lg btn-outline-primary px-5 mb-5">Subscribe to the No Rezone NTM Mailing List</a>
-                <p class="py-3">Web application designed and programmed by Nathan Getz.</p>
-                </div>
-            </footer>
-        <!--div class="vignette"></div-->
+            </form>
+            <p class="py-3">Web application designed and programmed by Nathan Getz.</p>
+            </div>
+        </footer>
+        <script>
+            (function() {
+                'use strict';
+                window.addEventListener('load', function() {
+                    var forms = document.getElementsByClassName('needs-validation');
+                    var validation = Array.prototype.filter.call(forms, function(form) {
+                        form.addEventListener('submit', function(event) {
+                            if (form.checkValidity() === false) {
+                                event.preventDefault();
+                                event.stopPropagation();
+                            }
+                            form.classList.add('was-validated');
+                        }, false);
+                    });
+                }, false);
+            })();
+        </script>
         <?php if (function_exists('customPageFooterScripts')){customPageFooterScripts();}?>
     </body>
 </html>
