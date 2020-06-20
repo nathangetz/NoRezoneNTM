5aa2072b9e09a6d285adef9b193f5febb7c23444
Nathan Getz <2036851@jeffcoschools.us>
2020-06-20 11:58:52 -0600

    fixed regex

--- footer.php
+++ footer.php
@@ -5,10 +5,10 @@
             <?php if (function_exists('customPageFooter')){
             customPageFooter();
             }?>
-            <form method="POST" action="mail.php" id="mail-sign-up" class="py-3 needs-validation" novalidate>
+            <form method="POST" action="mail.php" id="mail-sign-up" class="py-3">
                 <h3 class="text-light font-weight-lighter">To receive info and updates, subscribe to the No Rezone NTM mailing list:</h3>
-                <div class="input-group form-check my-3">
-                    <input class="form-control" name="email-address" type="email"  size="64" maxLength="64" required placeholder="Email address..." aria-label="Email address" pattern=".+@." title="Enter a valid email address.">
+                <div class="input-group my-3">
+                    <input class="form-control" name="email-address" type="email" required placeholder="Email address..." aria-label="Email address" pattern="\A[a-z0-9!#$%&'*+/=?^_‘{|}~-]+(?:\.[a-z0-9!#$%&'*+/=?^_‘{|}~-]+)*@(?:[a-z0-9](?:[a-z0-9-]*[a-z0-9])?\.)+[a-z0-9](?:[a-z0-9-]*[a-z0-9])?\z" title="Enter a valid email address.">
                     <div class="input-group-append">
                         <button class="btn btn-primary" type="submit"><span class="mx-2 mx-sm-3 mx-md-4 mx-lg-5">Subscribe</span></button>
                     </div>
@@ -18,23 +18,6 @@
             <p class="py-3">Web application designed and programmed by Nathan Getz.</p>
             </div>
         </footer>
-        <script>
-            (function() {
-                'use strict';
-                window.addEventListener('load', function() {
-                    var forms = document.getElementsByClassName('needs-validation');
-                    var validation = Array.prototype.filter.call(forms, function(form) {
-                        form.addEventListener('submit', function(event) {
-                            if (form.checkValidity() === false) {
-                                event.preventDefault();
-                                event.stopPropagation();
-                            }
-                            form.classList.add('was-validated');
-                        }, false);
-                    });
-                }, false);
-            })();
-        </script>
         <?php if (function_exists('customPageFooterScripts')){customPageFooterScripts();}?>
     </body>
 </html>
